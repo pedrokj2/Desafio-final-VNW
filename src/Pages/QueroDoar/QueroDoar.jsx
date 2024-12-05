9e76b3031ccb84dbd4206263c32fafa91af4cb89
Pedro Lucas <Pelukamedeiros@gmail.com>
2024-12-04 23:01:24 -0300

    ajuste no projeto

--- src/Pages/QueroDoar/QueroDoar.jsx
+++ src/Pages/QueroDoar/QueroDoar.jsx
@@ -1,23 +1,26 @@
-import S from './queroDoar.module.scss'
-import livro from '../../assets/livro.png'
+import S from "./queroDoar.module.scss";
+import livro from "../../assets/livro.png";
 
-export default function QueroDoar(){
-    return(
-        <section className={S.Principal}>
-            <section className={S.container}>
-                <h2>Por favor, preencha o formulário com suas informações e as informações do Livro</h2>
-                <form action="">
-                    <div>
-                        <img src={livro} alt="icone de um livro" />
-                        <h3>Informações do Livro</h3>
-                    </div>
-                    <input type="text" placeholder='Título' />
-                    <input type="text" placeholder='Categoria' />
-                    <input type="text" placeholder='Autor' />
-                    <input type="text" placeholder='Link da imagem' />
-                    <input type="submit" value="Doar"/>
-                </form>
-            </section>
-        </section>
-    )
+export default function QueroDoar() {
+  return (
+    <section className={S.Principal}>
+      <section className={S.container}>
+        <h2>
+          Por favor, preencha o formulário com suas informações e as informações
+          do Livro
+        </h2>
+        <form action="">
+          <div>
+            <img src={livro} alt="icone de um livro" />
+            <h3>Informações do Livro</h3>
+          </div>
+          <input type="text" placeholder="Título" />
+          <input type="text" placeholder="Categoria" />
+          <input type="text" placeholder="Autor" />
+          <input type="text" placeholder="Link da imagem" />
+          <input type="submit" value="Doar" />
+        </form>
+      </section>
+    </section>
+  )
 }
--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -1,27 +1,44 @@
-import S from './footer.module.scss'
-import face from '../../assets/face.png'
-import insta from '../../assets/insta.png'
-import linkedin from '../../assets/linkedin.png'
-import youtube from '../../assets/youtube.png'
-import twitter from '../../assets/img/twitter.png'
+import S from "./footer.module.scss";
+import face from "../../assets/face.png";
+import insta from "../../assets/insta.png";
+import linkedin from "../../assets/linkedin.png";
+import youtube from "../../assets/youtube.png";
+import twitter from "../../assets/img/twitter.png";
 
-
-export default function footer(){
-    return(
-        <footer>
-            <section className={S.boxSocial}>
-                <h3>4002-8922</h3>
-                <nav>
-                    <a href=""> <img src={face} alt="imagem da rede social facebook" /></a>
-                    <a href=""> <img src={insta} alt="imagem da rede social instagram" /></a>
-                    <a href=""> <img src={twitter} alt="imagem da rede social twitter" /></a>
-                    <a href=""> <img src={linkedin} alt="imagem da rede social de trabalho linkedin" /></a>
-                    <a href=""> <img src={youtube} alt="imagem da logo do youtube" /></a>
-                </nav>
-            </section>
-            <section className={S.boxTexto}>
-                <p>Layout desenvolvido pela Vai Na Web para fins educativos - 2024  </p>
-            </section>
-        </footer>
-    )
+export default function footer() {
+  return (
+    <footer>
+      <section className={S.boxSocial}>
+        <h3>4002-8922</h3>
+        <nav>
+          <a href="">
+            {" "}
+            <img src={face} alt="imagem da rede social facebook" />
+          </a>
+          <a href="">
+            {" "}
+            <img src={insta} alt="imagem da rede social instagram" />
+          </a>
+          <a href="">
+            {" "}
+            <img src={twitter} alt="imagem da rede social twitter" />
+          </a>
+          <a href="">
+            {" "}
+            <img
+              src={linkedin}
+              alt="imagem da rede social de trabalho linkedin"
+            />
+          </a>
+          <a href="">
+            {" "}
+            <img src={youtube} alt="imagem da logo do youtube" />
+          </a>
+        </nav>
+      </section>
+      <section className={S.boxTexto}>
+        <p>Layout desenvolvido pela Vai Na Web para fins educativos - 2024 </p>
+      </section>
+    </footer>
+  )
 }
